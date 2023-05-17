26bc11f410e5b808e6a06085b26082dab4558131
unknown <javidai@code.edu.az>
2023-05-18 02:26:51 +0400

    Add Tasks

---
+++ P135-Api-Onion/Repository/Migrations/20230517211454_AddingTables.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class AddingTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 493, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 493, DateTimeKind.Local).AddTicks(4));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 493, DateTimeKind.Local).AddTicks(5));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 492, DateTimeKind.Local).AddTicks(9838));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 492, DateTimeKind.Local).AddTicks(9856));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 492, DateTimeKind.Local).AddTicks(9858));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 11, 55, 328, DateTimeKind.Local).AddTicks(6294));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 11, 55, 328, DateTimeKind.Local).AddTicks(6299));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 11, 55, 328, DateTimeKind.Local).AddTicks(6300));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 11, 55, 328, DateTimeKind.Local).AddTicks(6130));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 11, 55, 328, DateTimeKind.Local).AddTicks(6144));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 11, 55, 328, DateTimeKind.Local).AddTicks(6146));
+        }
+    }
+}
---
+++ P135-Api-Onion/Repository/Migrations/20230517221839_AddDtoModels.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class AddDtoModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 2, 18, 39, 285, DateTimeKind.Local).AddTicks(1395));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 2, 18, 39, 285, DateTimeKind.Local).AddTicks(1398));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 2, 18, 39, 285, DateTimeKind.Local).AddTicks(1399));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 2, 18, 39, 285, DateTimeKind.Local).AddTicks(1226));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 2, 18, 39, 285, DateTimeKind.Local).AddTicks(1247));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 2, 18, 39, 285, DateTimeKind.Local).AddTicks(1249));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 493, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 493, DateTimeKind.Local).AddTicks(4));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 493, DateTimeKind.Local).AddTicks(5));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 492, DateTimeKind.Local).AddTicks(9838));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 492, DateTimeKind.Local).AddTicks(9856));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 18, 1, 14, 54, 492, DateTimeKind.Local).AddTicks(9858));
+        }
+    }
+}
